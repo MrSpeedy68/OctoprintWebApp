02b763a117ce374844f732f04fac01ddf0550ac1
MrSpeedy68 <mrspeedygamer123@gmail.com>
2022-03-29 15:08:00 +0100

    first commit

---
+++ ApiCalled.py
@@ -0,0 +1,18 @@
+
+import requests
+API_KEY = ""
+api_url = "http://192.168.1.16/api"
+
+auth_header = {'X-Api-Key': API_KEY}
+
+
+def get_nozzle_info():
+    nozzle_get = "/printer/bed"
+    return requests.get(api_url+nozzle_get, headers=auth_header)
+
+if __name__ == "__main__":
+    print(get_nozzle_info().text)
+
+
+
+
